f736c181b71230dde722220b8ff38e0912e2fd1d
melissa smashey <72240446+melissasmashey@users.noreply.github.com>
2021-03-13 13:09:00 -0500

    sat-update

--- static/chipotle.js
+++ static/chipotle.js
@@ -4,12 +4,21 @@ function search(){
     console.log("test");
     state = d3.select("#state").node().value;
     console.log(state);
-    d3.json("/data").then(data => {
+    var tbody = d3.select("tbody")
+    d3.json("/data", function(data){
+        tbody.html("")
         filtered_data = data.filter(d => d.state === state)
         console.log(filtered_data)
         // Erase the table
         // Build the table with filtered data
+        filtered_data.forEach(function(chipotle){
+            // console.log(select_date);
+            var row = tbody.append("tr");   
+            row.append("td").text(chipotle.location);
+            row.append("td").text(chipotle.address);
+        })
     })
 }
 
-d3.select("#state").on("click", search);
+d3.select("#form").on("submit", search);
+//form.on("submit", search);
